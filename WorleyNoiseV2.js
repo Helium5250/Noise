89c43c477bbca2d175a0d595887b0265dda8b503
codicate <codicate@gmail.com>
2021-03-10 01:26:40 -0500

    Weird stuff

--- WorleyNoiseV2.js
+++ WorleyNoiseV2.js
@@ -1,62 +1,107 @@
 const randRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-const canvas = document.querySelector('#canvas');
-console.log(canvas);
+function HSLToRGB(h, s, l) {
+  s /= 100;
+  l /= 100;
 
-const rSlider = document.querySelector('#r-slider');
-const gSlider = document.querySelector('#g-slider');
-const bSlider = document.querySelector('#b-slider');
-const brightnessSlider = document.querySelector('#brightness-slider');
+  const c = (1 - Math.abs(2 * l - 1)) * s;
+  const x = c * (1 - Math.abs((h / 60) % 2 - 1));
+  const m = l - c / 2;
+  let r = 0;
+  let g = 0;
+  let b = 0;
+
+  if (h >= 0 && h < 60) {
+    r = c; g = x; b = 0;
+  } else if (h >= 60 && h < 120) {
+    r = x; g = c; b = 0;
+  } else if (h >= 120 && h < 180) {
+    r = 0; g = c; b = x;
+  } else if (h >= 180 && h < 240) {
+    r = 0; g = x; b = c;
+  } else if (h >= 240 && h < 300) {
+    r = x; g = 0; b = c;
+  } else if (h >= 300 && h < 360) {
+    r = c; g = 0; b = x;
+  }
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+
+  return [r, g, b];
+}
+
+const canvas = document.querySelector('#canvas');
 class Canvas {
-  constructor(width, height, layer = 1) {
+  constructor(width, height, layer = 1, weirdX = 4, weirdY = 4) {
     this.width = width;
     this.height = height;
     this.layer = layer;
-    this.randPoint = [];
-    this.distToPoint = [];
+    this.weirdX = weirdX;
+    this.weirdY = weirdY;
 
     this.ctx = canvas.getContext('2d');
     this.ctx.canvas.width = width;
     this.ctx.canvas.height = height;
     this.data = this.ctx.createImageData(width, height);
-    this.denominator = this.width ** 2 / 255;
 
     for (let i = 0; i < this.data.data.length; i += 4) {
       this.data.data[i + 3] = 255;
     }
+
+    console.log(weirdX, weirdY);
   }
 
   generatePoint(num) {
-    this.distToPoint = [];
     this.randPoint = [];
     for (let i = 0; i < num; i++) {
       const x = randRange(0, this.width);
       const y = randRange(0, this.height);
       const z = randRange(0, this.layer);
-      this.randPoint.push({
-        x,
-        y,
-        z,
-      });
-      this.distToPoint.push(0);
+      this.randPoint.push({ x, y, z });
     }
     return this;
   }
 
-  draw(z, timestamp) {
-    const pitch = this.width * 4;
+  draw(z) {
+    const denominator = this.width ** 2 / 3 / 255;
+
     for (let x = 0; x < this.width; x++) {
       for (let y = 0; y < this.height; y++) {
+        const distToPoint = [];
+
         for (let i = 0; i < this.randPoint.length; i++) {
           const p = this.randPoint[i];
-          this.distToPoint[i] = (p.x - x) ** 2 + (p.y - y) ** 2;
+          distToPoint.push(Math.hypot(p.x - x, p.y - y, p.z - z));
+          // distToPoint.push((p.x - x) ** 2 + (p.y - y) ** 2 + (p.z - z) ** 2);
         }
-        const sortedPoint = this.distToPoint.sort((a, b) => a - b);
-        const grey = (sortedPoint[0] + sortedPoint[1] + sortedPoint[2]) / (this.denominator) + (brightnessSlider.value * 255);
 
-        this.data.data[y * pitch + x * 4 + 0] = grey * rSlider.value;
-        this.data.data[y * pitch + x * 4 + 1] = grey * gSlider.value;
-        this.data.data[y * pitch + x * 4 + 2] = grey * bSlider.value;
+        const sortedPoint = distToPoint.sort((a, b) => a - b);
+        const H = (distToPoint[0] / this.width) * 360 / 2;
+        const S = (distToPoint[2] / this.width) * 400;
+        const L = (distToPoint[1] / this.width) * 300;
+        const [R, G, B] = HSLToRGB(H, S, L);
+
+        // const R = (distToPoint[0] / this.width) * 256;
+        // const G = (distToPoint[2] / this.width) * 256;
+        // const B = (distToPoint[1] / this.width) * 256;
+
+        const coordinate = y * this.width * this.weirdX + x * this.weirdY;
+        this.data.data[coordinate + 0] = R;
+        this.data.data[coordinate + 1] = G;
+        this.data.data[coordinate + 2] = B;
+
+        this.data.data[coordinate + 4] = R;
+        this.data.data[coordinate + 5] = G;
+        this.data.data[coordinate + 6] = B;
+
+        this.data.data[coordinate + 8] = R;
+        this.data.data[coordinate + 9] = G;
+        this.data.data[coordinate + 10] = B;
+
+        this.data.data[coordinate + 12] = R;
+        this.data.data[coordinate + 13] = G;
+        this.data.data[coordinate + 14] = B;
       }
     }
     return this;
@@ -68,15 +113,14 @@ class Canvas {
 
   loopLayer() {
     const thisObj = this;
-
     let counter = 0;
 
-    function step(timestamp) {
+    function step() {
       if (counter > thisObj.layer) {
-        console.log('finished');
         return;
       }
-      thisObj.draw(counter, timestamp).show();
+
+      thisObj.draw(counter).show();
       counter++;
       window.requestAnimationFrame(step);
     }
@@ -84,9 +128,9 @@ class Canvas {
   }
 }
 
-const newCanvas = new Canvas(200, 200, 400);
+const newCanvas = new Canvas(100, 100, 100, randRange(1, 100), randRange(1, 100));
 
 function generate() {
-  newCanvas.generatePoint(randRange(10, 20)).loopLayer();
+  newCanvas.generatePoint(100).loopLayer();
 }
 generate();
